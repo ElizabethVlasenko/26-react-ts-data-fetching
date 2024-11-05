b31d7cd182087682d4730254dfd4fa4c98849eb1
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-05 11:28:35 +0000

    Add utility get function to fetch data

---
+++ src/util/http.ts
@@ -0,0 +1,10 @@
+export async function get(url: string) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = (await response.json()) as unknown; // can use zod library to validate data and get type
+  return data;
+}
